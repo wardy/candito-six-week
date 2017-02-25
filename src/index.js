fc22f9107d796ba300aa65fe564cf671405d8a19
wardy <christopher.ward2@bskyb.com>
2017-02-25 12:43:46 +0000

    basic app with simple routing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
+
 import App from './app.vue';
 
-const hi = () => { console.log(...arguments); }
+Vue.use(VueRouter);
+
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+
+const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar }
+]
+
+const router = new VueRouter({
+  routes
+})
+
 
 const app = new Vue({
   router,
